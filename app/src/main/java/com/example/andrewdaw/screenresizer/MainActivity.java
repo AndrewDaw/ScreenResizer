2a59442f5e422630d3d42b894952b2c75386e53f
Andrew Daw <andrew.daw1995@gmail.com>
2018-05-28 14:37:11 +1200

    Executes a hardcoded command "wm size 720x2180"

--- app/src/main/java/com/example/andrewdaw/screenresizer/MainActivity.java
+++ app/src/main/java/com/example/andrewdaw/screenresizer/MainActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -19,11 +25,49 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        //somestuffystuffDW
+        ArrayList<String>cmds = new ArrayList<String>();
+        cmds.add("wm size 720x1280");
+        final ArrayList<String> cmnds = cmds;
+
+
+        class C extends ExecuteAsRootBase {
+            @Override
+            protected ArrayList<String> getCommandsToExecute(){
+                return cmnds;
+            }
+
+
+        }
+
+        C.canRunRootCommands();
+        C ok = new C();
+        ok.execute();
+
+
+
+
+
     }
+
+//    public void runCommands(ArrayList<String> cmds){
+////        final ArrayList cmnds = cmds;
+////        new Thread(new Runnable() {
+////            public void run() {
+////
+////
+////
+////            }
+////        }).start();
+////    }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+
 }
