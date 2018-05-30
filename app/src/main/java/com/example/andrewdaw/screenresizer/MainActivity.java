e13e55012d15d6254d825613b34f740a96932b91
Andrew Daw <andrew.daw1995@gmail.com>
2018-05-30 19:20:54 +1200

    BROKEN Um did some things and forgot to commit BROKEN -having issues getting reference to a programatically created view -added a button for getting root permisions -tried to add an animation but having some kinda issues getting a reference to the view  ,which i need for later so i can dispose of the view once im done, currently using a dummy image   in place of the gif cause i thought it might be a GifView problem but its also not working with   an ImageView so yeahh mm

--- app/src/main/java/com/example/andrewdaw/screenresizer/MainActivity.java
+++ app/src/main/java/com/example/andrewdaw/screenresizer/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.andrewdaw.screenresizer;
 
+import android.app.ActionBar;
+import android.content.Context;
+import android.os.SystemClock;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -15,8 +22,13 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout cl;
+    ImageView unlockAni;
+    static int ahdee;
 
 
     // NDK is probably going to be useful, plus i wana play around with it
@@ -29,59 +41,107 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        unlockAni = new ImageView(this);
+        unlockAni.setImageResource(R.drawable.unlock_screen);
+        unlockAni.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
+
+        cl = (ConstraintLayout) findViewById(R.id.lay);
 
         // Example of a call to a native method
 //        TextView tv = (TextView) findViewById(R.id.sample_text);
 //        tv.setText(stringFromJNI());
 
 
-
-
         //STUFF starts here TODO: lots, lol
         //commands to be executed
 
 
+    }
 
-
-
-
-        //get root permissions
-        if(
-        ExecuteAsRootBase.canRunRootCommands()){
-            ImageView restricted = (ImageView) findViewById(R.id.ivRestricted);
-            restricted.setVisibility(View.GONE);
-            Button exec = (Button) findViewById(R.id.bExec);
-            exec.setVisibility(View.VISIBLE);
-            findViewById(R.id.tvNoRoot).setVisibility(View.GONE);
-        }
-
-
-
-
-
-
-
+    public void bExec(View view) {
+        TextView tv = (TextView) findViewById(R.id.tvRes);
+        String str = (String) tv.getText().toString();
+        executeCommands(str);
 
     }
 
-    public void bExec(View view){
-       TextView tv = (TextView)findViewById(R.id.tRes);
-       String str = (String) tv.getText().toString();
-        executeCommands(str);
+    //get root permissions and gets 'unlocks' app
+    public void bGetRoot(View view) {
+        //get root permissions and gets 'unlocks' app
+        if (
+                ExecuteAsRootBase.canRunRootCommands()) {
+            findViewById(R.id.ivRestricted).setVisibility(View.GONE);
+            findViewById(R.id.bExec).setVisibility(View.VISIBLE);
+            findViewById(R.id.tvNoRoot).setVisibility(View.GONE);
+            findViewById(R.id.bGetRoot).setVisibility(View.GONE);
+            displayAnimation();
+        }
+
+    }
+
+    public void displayAnimation() {
+        int id;
+        new Thread(new Runnable() {
+            int ids;
+
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+
+
+                        cl.addView(unlockAni);
+                        unlockAni.generateViewId();
+                        ahdee = unlockAni.getId();
+                                System.out.println("\n*\n*\n*\n*\n*\n*\n*\n*\n\n"+unlockAni.getId()+"\n*\n*\n*\n*\n*\n*\n*\n*\n\n");
+                        System.out.println("\n*\n*\n*\n*\n*\n*\n*\n*\n\n"+ahdee+"\n*\n*\n*\n*\n*\n*\n*\n*\n\n");
+
+                    }
+
+                });
+
+
+                try {
+            SystemClock.sleep(2000);
+                } catch (Exception e) {
+                    //and do nothing!
+                }
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+
+
+                                System.out.println("\n*\n*\n*\n##\n##\n##\n##\n##\n\n"+ahdee+"\n*\n*\n*\n*\n*\n*\n*\n*\n\n");
+                                findViewById(ahdee).setVisibility(View.GONE);
+                            }
+                        });
+
+
+                    }
+                });
+
+
+            }
+
+
+        }).start();
+
 
     }
 
 
     /*Executes a terminal command as root
-    *@cmd Command to be executed
+     *@cmd Command to be executed
      */
-    public void executeCommands(final String cmd){
+    public void executeCommands(final String cmd) {
         //implementing the class that can do root things
         class exeRoot extends ExecuteAsRootBase {
             @Override
-            protected ArrayList<String> getCommandsToExecute(){
-                return  new ArrayList<String>() {{
-                    add("wm size "+cmd);
+            protected ArrayList<String> getCommandsToExecute() {
+                return new ArrayList<String>() {{
+                    add("wm size " + cmd);
 
                 }};
             }
@@ -105,12 +165,6 @@ public class MainActivity extends AppCompatActivity {
 ////            }
 ////        }).start();
 ////    }
-
-
-
-
-
-
 
 
     /**
